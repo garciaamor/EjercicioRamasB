0f191e743d87773b797f5d5aded0614937292658
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-02-29 09:45:34 +0100

    commit devA

--- src/ejercicioramasb/ClasePro.java
+++ src/ejercicioramasb/ClasePro.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ejercicioramasb;
-
 /**
  *
  * @author jgarciaamor
  */
 public class ClasePro {
+    String frase="Cuando un tonto va por un camino, el camino se acaba y el tonto sigue";
     
+    public void visualizar(){
+        System.out.println(frase);
+    }
 }
--- src/ejercicioramasb/EjercicioRamasB.java
+++ src/ejercicioramasb/EjercicioRamasB.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package ejercicioramasb;
 
 /**
@@ -6,8 +12,11 @@ package ejercicioramasb;
  */
 public class EjercicioRamasB {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
     }
     
 }
